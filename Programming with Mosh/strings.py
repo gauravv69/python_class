406ae3c96f4247db09f5fd0e62e976fedbbd2be8
gauravv69 <gauravkadam2422@gmail.com>
2025-04-02 23:20:52 +0530

    Add type conversion and strings file in folder Mosh

---
+++ Programming with Mosh/strings.py
@@ -0,0 +1,22 @@
+course = '''
+Hi Gaurav,
+
+Here is our first email to you.
+
+Thank you, 
+The support team
+
+'''
+print(course)
+
+courses = 'Python for Beginners'
+another = courses[:]
+
+print(courses[-2])
+print(course[0])
+print(courses[0:3])
+print(courses[0:])
+print(courses[1:])
+print(courses[:5])
+
+print(another)
