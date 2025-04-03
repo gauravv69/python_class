ad6ebc0e0f2a04063e1f940c3c051d22b6adb8e5
gauravv69 <gauravkadam2422@gmail.com>
2025-04-03 13:03:06 +0530

    Add files of lesson formatted strings & string methods in the folder Mosh

---
+++ Programming with Mosh/string_methods.py
@@ -0,0 +1,7 @@
+course = 'Python for Beginners'
+
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course)
---
+++ Programming with Mosh/string_methods02.py
@@ -0,0 +1,11 @@
+course = 'Python for Beginners'
+
+print(course.find('P'))
+print(course.find('o'))
+print(course.find('Beginners'))
+
+print(course.replace('Beginners', 'Absolute Beginners'))
+print(course.replace('P', 'J'))
+
+print('Python' in course)
+print('python' in course)
