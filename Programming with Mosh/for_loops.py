0d0e15bb24db52393837df4035a2f899a58fa4fe
gauravv69 <gauravkadam2422@gmail.com>
2025-04-05 20:35:41 +0530

    Add files of lesson for & nested loops in the folder of Mosh

---
+++ Programming with Mosh/for_loops.py
@@ -0,0 +1,17 @@
+for item in 'Python':
+    print(item)
+
+for i in ['Mosh', 'John', 'Sarah']:
+    print(i)
+
+for numbs in [1, 2, 3, 4]:
+    print(numbs)
+
+for no in range(10):
+    print(no)
+
+for digits in range(5, 10):
+    print(digits)
+
+for digitss in range(5, 10, 2):
+    print(digitss)
---
+++ Programming with Mosh/nested_loops02.py
@@ -0,0 +1,11 @@
+numbers = [5, 2, 5, 2, 2]
+for x_cout in numbers:
+    print('x' * x_cout)
+
+
+nums = [5, 2, 5, 2, 2]
+for y_count in nums:
+    output = ''
+    for count in range(y_count):
+        output += 'y'
+    print(output)
