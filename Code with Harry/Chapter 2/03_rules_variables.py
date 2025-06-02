cd939ba2a76079b0201d871b6c96e806d2e64daa
gauravv69 <gauravkadam2422@gmail.com>
2025-06-03 00:39:07 +0530

    Add all files of chapter 2 in the folder of Harry

---
+++ Code with Harry/Chapter 2/03_rules_variables.py
@@ -0,0 +1,13 @@
+a = 23
+
+aaa = 345
+
+harry = 34
+
+gaurav = 54
+
+_gaurav = 34
+
+# @gaurav = 56 # Invalid due to @symbol
+
+# s@meer # Invalid due to @symbol
---
+++ Code with Harry/Chapter 2/04_operators.py
@@ -0,0 +1,42 @@
+# Arithmetic Operators
+a = 34
+b = 4
+c = a + b
+print(c)
+
+# Assignment Operators
+a = 4 - 2 # Assign 4 - 2 in a
+print(a)
+b = 6
+# b += 3  # Increment the value of b by 3 and then assign it to b
+b -= 3  # Decrement the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+
+d = 5>=5
+print(d)
+
+e = 5!=5
+print(e)
+
+
+# Logical operators
+
+f = True or False
+print(f)
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is  ", True or  True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and'
+print("True and False is ", True and False)
+print("True and True is  ", True and  True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(False))
+print(not(True))
